cd4941212957e76672f5dc31182e49ed94476d90
somos <mmelozano96@gmail.com>
2026-04-28 02:10:18 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function enterGarden() {
   document.getElementById("game").innerHTML = `
-    <img src="yoda-pixel.png" class="npc-sprite" alt="Pixel Yod'Da" />
+    <img src="yodda-pixel.png" class="npc-sprite" alt="Pixel Yod'Da" />
 
     <h2>Yod'Da appears...</h2>
 
